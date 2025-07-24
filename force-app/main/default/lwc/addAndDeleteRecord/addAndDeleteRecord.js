f63d60e4a1bb0ba1158335d4ddaa40c277ff6c00
Nishith Yadav <nishithyadav.140@gmail.com>
2025-07-25 01:28:39 +0530

    Added new LWC component and associated Apex Class

---
+++ force-app/main/default/lwc/addAndDeleteRecord/addAndDeleteRecord.js
@@ -0,0 +1,45 @@
+import { LightningElement, wire, track } from 'lwc';
+import getContacts from '@salesforce/apex/ContactController.getContacts';
+import addRecord from '@salesforce/apex/ContactController.addRecord';
+import DeleteRecord from '@salesforce/apex/ContactController.DeleteRecord';
+
+export default class AddAndDeleteRecord extends LightningElement {
+
+    records;
+    error;
+    @track nm;
+    @track em;
+
+    @wire(getContacts)
+    wiredContacts({data, error}) {
+        if(data) {
+            this.records = data;
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.records = undefined;
+        }
+    }
+
+    handleAddClick() {
+        addRecord({
+            nm: this.nm,
+            em: this.em
+        });
+    }
+
+    handleDeleteClick() {
+
+        DeleteRecord({
+            Id: this.detail.Id
+        });
+    }
+
+    handleNameChange() {
+        nm = this.detail.value;
+    }
+
+    handleEmailChange() {
+        em = this.detail.value;
+    }
+}
